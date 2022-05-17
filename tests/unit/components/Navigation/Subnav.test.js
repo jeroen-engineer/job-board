88381f3c78cd61c43de6af36588084ee4a072ebb
jeroen-engineer <jeroentheunissen@protonmail.com>
2022-05-17 12:08:55 +0200

    Created job serach form and besides updated foler structure for components

--- tests/unit/components/Navigation/Subnav.test.js
+++ tests/unit/components/Navigation/Subnav.test.js
@@ -1,5 +1,5 @@
 import { mount } from "@vue/test-utils";
-import Subnav from "@/components/Subnav";
+import Subnav from "@/components/Navigation/Subnav";
 
 describe("Subnav", () => {
   describe("when user is on job page", () => {
