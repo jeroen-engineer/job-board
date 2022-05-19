310e22a6202ab9f7d6193ffa880ac055cfdf67a2
jeroen-engineer <jeroentheunissen@protonmail.com>
2022-05-19 16:30:48 +0200

    Build Job listings component, job listing component, installed json- server, installed axios, created db.json with mock jobs, tested job listing and job listings

---
+++ tests/unit/components/JobResults/JobListing.test.js
@@ -0,0 +1,60 @@
+import { mount, RouterLinkStub } from "@vue/test-utils";
+
+import JobListing from "@/components/JobResults/JobListing.vue";
+
+describe("JobListing", () => {
+  const createJobProps = (jobProps = {}) => ({
+    title: "Vue Developer",
+    organization: "bobo",
+    locations: ["New York"],
+    minimumQualifications: ["SQL"],
+
+    ...jobProps,
+  });
+  const createConfig = (jobProps) => ({
+    props: {
+      job: {
+        ...jobProps,
+      },
+    },
+    global: {
+      stubs: {
+        "router-link": RouterLinkStub,
+      },
+    },
+  });
+  it("renders job title", () => {
+    const jobProps = createJobProps({ title: "Vue Programmer" });
+    const wrapper = mount(JobListing, createConfig(jobProps));
+    expect(wrapper.text()).toMatch("Vue Programmer");
+  });
+  it("renders job organization", () => {
+    const jobProps = createJobProps({ organization: "bobo" });
+    const wrapper = mount(JobListing, createConfig(jobProps));
+    expect(wrapper.text()).toMatch("bobo");
+  });
+
+  it("render job locations", () => {
+    const jobProps = createJobProps({ locations: ["Orlando", "Jacksonville"] });
+    const wrapper = mount(JobListing, createConfig(jobProps));
+    expect(wrapper.text()).toMatch("Orlando");
+    expect(wrapper.text()).toMatch("Jacksonville");
+  });
+  it("render job qualifications", () => {
+    const jobProps = createJobProps({
+      minimumQualifications: ["Code", "Develop"],
+    });
+    const wrapper = mount(JobListing, createConfig(jobProps));
+    expect(wrapper.text()).toMatch("Code");
+    expect(wrapper.text()).toMatch("Develop");
+  });
+  it("links to individual job's page", () => {
+    const jobProps = createJobProps({
+      id: 15,
+    });
+    const wrapper = mount(JobListing, createConfig(jobProps));
+    const jobPageLink = wrapper.findComponent(RouterLinkStub);
+    const toProp = jobPageLink.props("to");
+    expect(toProp).toBe("/jobs/results/15");
+  });
+});
---
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -0,0 +1,21 @@
+import { shallowMount, flushPromises } from "@vue/test-utils";
+import axios from "axios";
+jest.mock("axios");
+
+import JobListings from "@/components/JobResults/JobListings.vue";
+
+describe("JobListings", () => {
+  it("fetches jobs", () => {
+    axios.get.mockResolvedValue({ data: [] });
+    shallowMount(JobListings);
+    expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
+  });
+  it("created a job listing for each received job", async () => {
+    axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+    shallowMount(JobListings);
+    const wrapper = shallowMount(JobListings);
+    await flushPromises();
+    const jobListings = wrapper.findAll("[data-test='job-listing']");
+    expect(jobListings).toHaveLength(15);
+  });
+});
